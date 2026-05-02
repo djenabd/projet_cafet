839f8747aa37d16da538bc6b40be6edc1cb2f96f
Djenab Dramé <djenab.drame@ensea.fr>
2026-05-02 23:26:53 +0200

    dernières réparations

--- cafeteria/settings.py
+++ cafeteria/settings.py
@@ -101,6 +101,7 @@ LOGIN_REDIRECT_URL = 'home'
 
 CAS_REDIRECT_URL = 'home'
 
+#pas sur
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend', 
     'django_cas_ng.backends.CASBackend',         
